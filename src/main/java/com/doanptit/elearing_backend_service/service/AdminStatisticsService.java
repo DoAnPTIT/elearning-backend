60e2f4c0c7d0136779899d33c80eb1004528a137
hvmhieu2003 <hvmhieu2003@gmail.com>
2025-10-02 20:13:46 +0700

    Fix api adminStatistic data type

--- src/main/java/com/doanptit/elearing_backend_service/service/AdminStatisticsService.java
+++ src/main/java/com/doanptit/elearing_backend_service/service/AdminStatisticsService.java
@@ -3,5 +3,5 @@ package com.doanptit.elearing_backend_service.service;
 import com.doanptit.elearing_backend_service.dto.res.AdminStatisticsDto;
 
 public interface AdminStatisticsService {
-    AdminStatisticsDto getStatistics();
+    AdminStatisticsDto getStatisticsQuantityByRole();
 }
