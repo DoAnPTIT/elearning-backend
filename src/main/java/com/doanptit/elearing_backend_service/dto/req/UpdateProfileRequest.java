6a27abed254d90a0570afb4aba7e28e7351f81b3
hvmhieu2003 <hvmhieu2003@gmail.com>
2025-10-10 19:56:15 +0700

    fix validate update-profile & user-request

--- src/main/java/com/doanptit/elearing_backend_service/dto/req/UpdateProfileRequest.java
+++ src/main/java/com/doanptit/elearing_backend_service/dto/req/UpdateProfileRequest.java
@@ -8,14 +8,10 @@ import lombok.Data;
 public class UpdateProfileRequest {
 
     @NotBlank(message = "Họ không được để trống.")
-    @Size(max = 50, message = "Họ không được vượt quá 50 ký tự.")
+    @Size(max = 20, message = "Họ không được vượt quá 20 ký tự.")
     private String firstname;
 
-    @NotBlank(message = "Tên không được để trống.")
-    @Size(max = 50, message = "Tên không được vượt quá 50 ký tự.")
     private String lastname;
 
-    // Tạm thời chỉ là link ảnh, sau này sẽ cập nhật chức năng upload file
-    @Size(max = 255, message = "Đường dẫn ảnh quá dài.")
     private String image;
 }
